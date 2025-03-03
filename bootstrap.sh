ac53953b13cf875ae0049a26d86c4e50d98fd5db
Jakub Mandula <jakub.mandula@pbl.ee.ethz.ch>
2025-03-03 19:26:17 +0100

    add everythign to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,7 +18,7 @@ if [ ! -d ~/.oh-my-zsh ]; then
 fi
 
 # List of directories to stow (excluding the script itself)
-MODULES=(zsh git emacs bash i3 tmux nvim vim nano)
+MODULES=(zsh alacritty git emacs bash i3 tmux nvim vim nano btop htop)
 
 # Stow each module
 for module in "${MODULES[@]}"; do
