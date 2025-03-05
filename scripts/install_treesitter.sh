98c3a5fec396bbc831c3886b8baab6d444877e37
Jakub Mandula <jakub.mandula@pbl.ee.ethz.ch>
2025-03-05 23:49:08 +0100

    add treesitter install script

---
+++ scripts/install_treesitter.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+curl -L -o tree-sitter.gz "https://github.com/tree-sitter/tree-sitter/releases/download/v0.25.3/tree-sitter-linux-x64.gz" && \
+gunzip tree-sitter.gz && \
+chmod +x tree-sitter && \
+mv tree-sitter ~/.local/bin/
+
