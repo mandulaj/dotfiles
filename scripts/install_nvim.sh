61c073a96cb7f99e42b85799b160752e97f4684b
Jakub Mandula <jakub.mandula@pbl.ee.ethz.ch>
2025-03-05 23:07:25 +0100

    add nvim and nvm install scripts

---
+++ scripts/install_nvim.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo rm -rf /opt/nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+rm nvim-linux-x86_64.tar.gz
---
+++ scripts/install_nvm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
+
+nvm install --lts
