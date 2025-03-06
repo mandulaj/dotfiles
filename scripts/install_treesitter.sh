2bc7c5b12b89553f2cf20fa00f4fc377b6d2085f
Jakub Mandula <jakub.mandula@pbl.ee.ethz.ch>
2025-03-06 05:31:24 +0000

    create .local/bin if it doesn't exit

--- scripts/install_treesitter.sh
+++ scripts/install_treesitter.sh
@@ -4,5 +4,6 @@
 curl -L -o tree-sitter.gz "https://github.com/tree-sitter/tree-sitter/releases/download/v0.25.3/tree-sitter-linux-x64.gz" && \
 gunzip tree-sitter.gz && \
 chmod +x tree-sitter && \
+mkdir -p ~/.local/bin
 mv tree-sitter ~/.local/bin/
 
