2745c37051081757fadecbd6b870879d0365df10
Jakub Mandula <jakub.mandula@pbl.ee.ethz.ch>
2025-11-25 11:28:37 +0100

    add ssss upgrades

--- bin/ssss.sh
+++ bin/ssss.sh
@@ -1,12 +1,33 @@
 #! /bin/bash
 
-if [ "$1" == "-h" ]; then
-	echo "ssss [-x] t n filename"
+
+THREASHOLD=${1:-3}
+N_PARTS=${2:-5}
+
+
+if [ "$THREASHOLD" == "-h" ]; then
+	echo "ssss t n [-x] filename"
+	echo "  -x : indicates that the file is binary and should be handled with xxd"
 	exit
 fi
 
-if [ "$1" == "-x" ]; then
-	xxd -p $4 | tr -d "\n" | ssss-split -w $4 -Q -t $2 -n $3 -x | split -l 1 - secret-$4
+
+
+if [ "$3" == "-x" ]; then
+	FILE_NAME=${4}
+	USE_XXD=true
 else
-	cat $3 |  ssss-split -w $3 -Q -t $1 -n $2 | split -l 1 - secret-$3
+	FILE_NAME=${3}
+	USE_XXD=false
+fi
+
+if [ "$FILE_NAME" == "" ]; then
+	echo "Please provide a file name"
+	exit 1
+fi
+
+if [ "$USE_XXD" == "true" ]; then
+	xxd -p $FILE_NAME | tr -d "\n" | ssss-split -w $FILE_NAME -Q -t $THREASHOLD -n $N_PARTS -x | split -d -l 1 - secret-$FILE_NAME-
+else
+	cat $FILE_NAME |  ssss-split -w $FILE_NAME -Q -t $THREASHOLD -n $N_PARTS | split -d -l 1 - secret-$FILE_NAME-
 fi
