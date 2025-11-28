3448310f8f3ad12969dd7ba1bd899e98dadda09c
Jakub Mandula <jakub.mandula@pbl.ee.ethz.ch>
2025-11-28 12:14:18 +0100

    fix some sway lock issues

--- bin/sway-lock-script.sh
+++ bin/sway-lock-script.sh
@@ -1,19 +1,31 @@
 #!/bin/sh
 # ~/.local/bin/sway-lock-script.sh
 
+DO_TIMEOUT=${1:-true}
+
+# GUARD CLAUSE:
+# If swaylock is already running, exit immediately.
+# This prevents the system-wide swayidle from spawning multiple locks.
+if pgrep -x "swaylock" > /dev/null; then
+    exit 0
+fi
+
+
+
 # 1. Start a temporary swayidle process in the background.
 #    - Turn off screen after 5 seconds of inactivity
 #    - Turn it back on when activity resumes
+if [ "$DO_TIMEOUT" = true ]; then
 swayidle -w \
    timeout 5 'swaymsg "output * dpms off"' \
    resume 'swaymsg "output * dpms on"' &
+fi
 
 # Capture the Process ID (PID) of the swayidle command we just ran
 IDLE_PID=$!
 
+trap "kill $IDLE_PID" EXIT
+
 # 2. Lock the screen immediately
 #    -f ensures it locks before processing further
-swaylock --clock --screenshot --effect-pixelate 30x30 --effect-vignette 0.5:1
-# 3. Kill the temporary swayidle process
-#    This runs only *after* swaylock finishes (when you unlock the computer)
-kill $IDLE_PID
+swaylock --clock --screenshot --effect-vignette 0.5:1 --effect-pixelate 30x30
